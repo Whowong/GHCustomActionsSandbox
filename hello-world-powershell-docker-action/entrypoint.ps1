b6b8f729064d062843a33c96352612fd95d08780
Whowong <54148527+Whowong@users.noreply.github.com>
2021-12-30 15:09:00 -0800

    Update entrypoint.ps1

--- hello-world-powershell-docker-action/entrypoint.ps1
+++ hello-world-powershell-docker-action/entrypoint.ps1
@@ -2,4 +2,4 @@ $name = $Env:INPUT_WHO_TO_GREET
 $message = "Hello $name"
 Write-Output "$message, this is a standard output"
 
-Write-Output "::set-output name=GREETING::$message"
+Write-Output "::set-output name=GREET::$message"
