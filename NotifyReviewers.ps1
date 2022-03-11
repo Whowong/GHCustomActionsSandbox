cac27b757259e2a233b267fde75539acbb8d2d5a
Whowong <ahuang05@hotmail.com>
2022-03-11 11:31:00 -0800

    Adding auth to get reviewers

--- NotifyReviewers.ps1
+++ NotifyReviewers.ps1
@@ -16,15 +16,21 @@ $URI = "https://api.github.com/repos/$ownerRepo/pulls/$PRID/requested_reviewers"
 $headers = @{
     'content-type' = 'application/json'
     'authorization'  = "Bearer $Env:SECRETS_GITHUB_TOKEN }}"
-    # Local Dev Parameters
-    #'Accept'         = "application/vnd.github.v3+json"
-    #'Authorization'  = "token $Env:SECRETS_GITHUB_TOKEN }}"
 }
+
+Write-Output "Here are the headers: $headers"
+
+# Local Dev Parameters
+# $headers = @{
+    
+#     'Accept'         = "application/vnd.github.v3+json"
+#     'Authorization'  = "token "
+# }
 
 # Getting the reviewers in the PR
 try {
     Write-Output "Checking $uri to get list of reviewers"
-    $users = (Invoke-WebRequest -Uri $URI).content | ConvertFrom-Json
+    $users = (Invoke-WebRequest -Uri $URI -Headers $headers).content | ConvertFrom-Json
 }
 catch {
     Write-Error "Failed to find reviewers, this could be cause if you do not have reviewers on this PR" -ErrorAction Stop
@@ -47,6 +53,5 @@ $body = $body | ConvertTo-Json
 $commentURI = "https://api.github.com/repos/$ownerRepo/issues/$PRID/comments"
 
 Write-Output "Posting to $commentURI"
-Write-Output "Here are the headers: $headers"
 
 Invoke-RestMethod -Method Post -Headers $headers -Uri $commentURI -Body $body
